94c04f0a1311ddfa51a4068ce90d6f22be3c4197
notmake <131246666+notmake@users.noreply.github.com>
2023-07-22 19:46:50 +0800

    Revert "first use github"
    This reverts commit 2a472892b0029858f54f786353380dec11450147.

--- 2.1urllib代码.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-import urllib.parse
-
-data = bytes(urllib.parse.urlencode({'name': 'germey'}), encoding='utf-8')
-response = urllib.request.urlopen("https://www.httpbin.org/post", data=data)
-print(response.read().decode('utf-8'))
